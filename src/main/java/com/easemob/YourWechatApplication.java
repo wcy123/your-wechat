e40332c30245d04b88e4ad3ab10bbcb371d17ea1
Wang Chunye <wcy123@gmail.com>
2017-01-14 10:19:26 +0800

    初始的空架子

---
+++ src/main/java/com/easemob/YourWechatApplication.java
@@ -0,0 +1,12 @@
+package com.easemob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourWechatApplication.class, args);
+	}
+}
